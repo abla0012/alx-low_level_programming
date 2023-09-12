252f072c00cda3f7629ae8544461c029fbe6c9a8
abla0012 <abliabla343@gmail.com>
2023-09-12 12:37:31 -0700

    the 12 commmit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This is a message with proper grammar.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	    /* This is a comment */
+	    printf("with proper grammar, but the outcome is a piece of art,\n");
+	    return (0);
+}
+

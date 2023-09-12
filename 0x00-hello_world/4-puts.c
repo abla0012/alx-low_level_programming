26676c5ff4aa8787fb00c13b308457841c11f239
abla0012 <abliabla343@gmail.com>
2023-09-12 12:07:43 -0700

    the 110 commmit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: This is the main function that prints a message.
- *     *
- *      * Return: 0 on success
- *       */
+ * main - Entry point
+ *
+ * Description: This is the main function that prints a message.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	    puts("\"Programming is like building a multilingual puzzle");
-	        return (0);
+	    return (0);
 }
-

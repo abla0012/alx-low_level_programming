b81e60f9f5bc80ec9db2ca95b09df2e6bb53c1fe
abla0012 <abliabla343@gmail.com>
2023-09-13 14:47:12 -0700

    task 002

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,14 @@
 void print_alphabet_x10()
 {
 	int i;
+	char c;
 
 	for(i = 0; i < 9; i++)
 	{
-		print_alphabet();
-         	_putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
 	}
 }
